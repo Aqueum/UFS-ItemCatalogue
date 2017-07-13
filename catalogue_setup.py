4bce3acf3efc21fce73fee0d175be32441811402
Aqueum <martin@aqueum.com>
2017-07-13 23:21:47 +0100

    Document catalogue database setup

--- catalogue_setup.py
+++ catalogue_setup.py
@@ -3,9 +3,11 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
 
+# construct a base class for declarative class definitions
 Base = declarative_base()
 
 
+# build the category table
 class Category(Base):
     __tablename__ = 'category'
     id = Column(Integer, primary_key=True)
@@ -13,16 +15,18 @@ class Category(Base):
     description = Column(String)
 
 
+# build the item table
 class Item(Base):
     __tablename__ = 'item'
     id = Column(Integer, primary_key=True)
     name = Column(String(64), nullable=False)
     description = Column(String)
     image = Column(String)
+    # establish relationship between item and category tables
     category_id = Column(Integer, ForeignKey('restaurant.id'))
     relationship = relationship(Category)
 
 
+# Create the database engine and database
 engine = create_engine('sqlite:///catalogue.db')
-
 Base.metadata.create_all(engine)
