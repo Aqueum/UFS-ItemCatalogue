76dec7afc8c940e4db046c7294f81e08a2abc391
Aqueum <martin@aqueum.com>
2017-07-15 22:36:16 +0100

    Move facebook login to separate page otherwise we get a new auth token for every page change

--- catalogue.py
+++ catalogue.py
@@ -14,6 +14,12 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 
+# show login page
+@app.route("/login/")
+def login_page():
+    return render_template('fbLogin.html')
+
+
 # show list of categories
 @app.route("/")
 @app.route("/categories/")
